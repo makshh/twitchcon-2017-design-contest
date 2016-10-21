cd593b5d1794f50616c88a8a88eabc604598b1d3
MC <m4ks@o2.pl>
2016-10-21 22:57:29 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# [Contest – Build TwitchCon 2017 Landing Page]
+
+My Twitch username: m4kshh
+My Discord username: MaxHH
+
+This contest is intended as a learning experience, and I will respect any requests by Twitch staff to remove this project.
+
+## What have I learned?
+
+* manipulating the DOM using pure JavaScript (without jQuery)
+* other vanilla JS funcionalities (especially events)
+* making better use of `tabindex` attribute
+* improving css transitions performance (see article in credits)
+* using clip-rect() CSS property
+* using some techniques related to SVG in Illustrator
+
+## Why are you describing your project? You wanna win, huh?
+
+Not really, I think the purpose of this contest is to learn something new (maybe someone will learn something from my landing page). Secondly, I'm learning English, so more I use it - more I learn. I know it's bad :D
+
+## Techniques used in this project
+
+### HTML
+
+* tabindex for keyboard navigation
+
+### CSS
+
+* transitions, transforms and animations
+* `calc()` function
+* `clip-rect()` for SVG animation
+* CSS3 selectors and pseudo selectors (like `~`, `+`, `:nth-child()`, `:invalid`, `:not()`)
+* media queries
+* pseudo elements (`:before` and `:after`)
+* [padding hack]
+* vh units
+* normalize.css (from Bootstrap 3.3.7)
+* `@font-face`
+
+### LESS
+
+* nesting selectors
+* variables (for colors)
+* `shade()` and `tint()` functions
+
+### JavaScript
+
+* elements - page loader, mobile menu, form submission, social links hover/focus effect, menu links hover/focus effect
+* events listeners
+* manipulating DOM
+* add/remove/toggle classes
+* forEach, style, setAttribute
+
+### Other tools
+
+* [Sublime Text 3] - sophisticated text editor for code, markup and prose
+* [CodeKit] - helps you build websites faster and better (compiling LESS files, auto-refresh browsers, minifying files, autoprefixer etc.)
+* [compressor.io] - compress and optimize your images
+* Adobe Photoshop (darkening background image) and Adobe Illustrator (SVG logo)
+
+### Graphics
+
+* modified SVG TwitchCon 2016 logo from [TwitchCon 2016 website]
+* Twitch Glitch logo from [Brand Assets - Twitch] - for favicon
+* [Background image]
+
+## Browser support
+
+* IE10+ and all major mobile and desktop browsers
+
+## Page speed test
+
+* [GTMetrix] - total page size - 225KB, page load time - ~0,7s
+
+## Credits
+
+* [Lato font] by [Łukasz Dziedzic]
+* [Exo font] by [Natanael Gama]
+* [How to achieve 60 FPS animations with CSS3] by [José Rosário]
+* [CSShake] by [@elrumordelaluz]
+* [Glitch Effect on Text / Images / SVG] by [Chris Coyier]
+* [IcoMoon App]
+* [Favic-o-Matic]
+* [Don Hansen - DonTheDeveloper]
+* [Emilis Tobulevicius - Tsukle]
+* [Steven | DevBowser]
+
+[Exo font]: <https://fonts.google.com/specimen/Exo>
+[Natanael Gama]: <http://ndiscovered.com/>
+[José Rosário]: <https://medium.com/@jose.rosario>
+[Sublime Text 3]: <https://www.sublimetext.com/3>
+[compressor.io]: <https://compressor.io/>
+[CodeKit]: <https://incident57.com/codekit/>
+[Brand Assets - Twitch]: <https://www.twitch.tv/p/brand-assets>
+[Background image]: <https://www.pexels.com/photo/people-rasing-hands-during-night-time-66463/>
+[Contest – Build TwitchCon 2017 Landing Page]: <http://blog.donthedeveloper.tv/2016/10/09/contest-build-twitchcon-2017-landing-page/>
+[DonTheDeveloper]: <https://www.twitch.tv/donthedeveloper>
+[padding hack]: <http://andyshora.com/css-image-container-padding-hack.html>
+[TwitchCon 2016 website]: <http://www.twitchcon.com/>
+[GTMetrix]: <https://gtmetrix.com/reports/doncsscontest.herokuapp.com/9bwzEr6K>
+[CSShake]: <https://elrumordelaluz.github.io/csshake/>
+[@elrumordelaluz]: <https://twitter.com/elrumordelaluz>
+[Glitch Effect on Text / Images / SVG]: <https://css-tricks.com/glitch-effect-text-images-svg/>
+[Chris Coyier]: <http://chriscoyier.net/>
+[Lato font]: <https://fonts.google.com/specimen/Lato>
+[Łukasz Dziedzic]: <http://www.lukaszdziedzic.eu/>
+[IcoMoon App]: <https://icomoon.io/app/>
+[Don Hansen - donthedeveloper]: <https://twitter.com/DeveloperDonTV>
+[Emilis Tobulevicius - Tsukle]: <https://twitter.com/tsukle>
+[Steven | DevBowser]: <https://twitter.com/devbowser>
+[Favic-o-Matic]: <http://www.favicomatic.com/>
+[How to achieve 60 FPS animations with CSS3]: <https://medium.com/outsystems-experts/how-to-achieve-60-fps-animations-with-css3-db7b98610108#.ozo0k9jb7>
---
+++ main.js
@@ -0,0 +1,426 @@
+// Vanilla JS version (4506 bytes minified), jQuery version - see below
+
+(function() {
+
+  'use strict';
+
+  // Page loader
+
+  window.addEventListener('load', function() {
+
+    var body = document.getElementsByTagName('body')[0],
+      loader = body.getElementsByClassName('loader-wrapper')[0];
+
+    setTimeout(function() {
+      loader.style.opacity = '0';
+      loader.style.visibility = 'hidden';
+      body.style.overflowY = 'auto';
+    }, 500);
+
+  });
+
+  // On DOM ready
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+    // Variables
+
+    var body = document.getElementsByTagName('body')[0],
+      sidebar = body.getElementsByClassName('sidebar')[0],
+      sidebarToggle = body.getElementsByClassName('sidebar-toggle')[0],
+      menu = sidebar.getElementsByTagName('ul')[0],
+      menuElements = menu.getElementsByTagName('li'),
+      menuElementLine = menu.getElementsByClassName('sidebar-link-line')[0],
+      menuActiveElement = menu.getElementsByClassName('active')[0],
+      socialButtons = body.querySelectorAll('.social > a'),
+      form = body.getElementsByTagName('form')[0],
+      input = form.querySelectorAll('input[type="email"]')[0],
+      submit = form.querySelectorAll('input[type="submit"]')[0];
+
+    // Close sidebar on resize (if browser width > 992px)
+
+    window.addEventListener('resize', function() {
+
+      if (window.outerWidth > 992) {
+        if (body.classList) {
+          body.classList.remove('sidebar-opened');
+        } else {
+          body.className = body.className.replace(new RegExp('(^|\\b)' + 'sidebar-opened'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+      }
+
+    }, false);
+
+    // Mobile menu toggle
+
+    sidebarToggle.addEventListener('click', function() {
+
+      if (sidebar.classList) {
+        sidebar.classList.add('active');
+      } else {
+        sidebar.className += ' ' + 'active';
+      }
+
+      var className = 'sidebar-opened';
+
+      if(body.classList) {
+        body.classList.toggle(className);
+      } else {
+        var classes = body.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+
+        if (existingIndex >= 0) {
+          classes.splice(existingIndex, 1);
+        }
+        else {
+          classes.push(className);
+        }
+        body.className = classes.join(' ');
+      }
+
+    }, false);
+
+    var removeActiveClassAfterTransition = function() {
+
+      if (this.classList) {
+        this.classList.remove('active');
+      } else {
+        this.className = this.className.replace(new RegExp('(^|\\b)' + 'active'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+      }
+
+    };
+
+    sidebar.addEventListener('webkitTransitionEnd', removeActiveClassAfterTransition, false);
+    sidebar.addEventListener('transitionEnd', removeActiveClassAfterTransition, false);
+
+    // Close mobile menu on clicking/tapping outside
+
+    function closeSidebarOutside() {
+
+      if(event.target.classList.contains('sidebar-overlay')) {
+
+        if (sidebar.classList) {
+          sidebar.classList.add('active');
+        } else {
+          sidebar.className += ' ' + 'active';
+        }
+
+        var className = 'sidebar-opened';
+
+        if((' ' + body.className + ' ').replace(/[\n\t]/g, ' ').indexOf(className) > -1) {
+          if (body.classList) {
+            body.classList.remove(className);
+          } else {
+            body.className = body.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+          }
+        }
+
+      }
+
+    }
+
+    document.addEventListener('mouseup', closeSidebarOutside, false);
+    document.addEventListener('touchend', closeSidebarOutside, false);
+
+    // Close mobile menu on ESC
+
+    document.addEventListener('keydown', function(event) {
+
+      event = event || window.event;
+      var isEscape = false;
+      if ('key' in event) {
+        isEscape = (event.key === 'Escape' || event.key === 'Esc');
+      } else {
+        isEscape = (event.keyCode === 27);
+      }
+      if (isEscape) {
+
+        if (sidebar.classList) {
+          sidebar.classList.add('active');
+        } else {
+          sidebar.className += ' ' + 'active';
+        }
+
+        var className = 'sidebar-opened';
+
+        if((' ' + body.className + ' ').replace(/[\n\t]/g, ' ').indexOf(className) > -1) {
+          if (body.classList) {
+            body.classList.remove(className);
+          } else {
+            body.className = body.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+          }
+        }
+
+      }
+
+    }, false);
+
+    // Menu link hover/focus effect
+
+    Array.prototype.forEach.call(menuElements, function(el) {
+      el.addEventListener('mouseover', function() {
+        menuElementLine.style.top = this.offsetTop + 'px';
+      }, false);
+      el.getElementsByTagName('a')[0].addEventListener('focus', function() {
+        menuElementLine.style.top = this.offsetTop + 'px';
+      }, false);
+      el.addEventListener('mouseout', function() {
+        menuElementLine.style.top = menuActiveElement.offsetTop + 'px';
+      }, false);
+      el.getElementsByTagName('a')[0].addEventListener('focusout', function() {
+        menuElementLine.style.top = menuActiveElement.offsetTop + 'px';
+      }, false);
+    });
+
+    // Social button hover/focus effect
+
+    Array.prototype.forEach.call(socialButtons, function(el) {
+
+      var textWidth = el.getElementsByClassName('text')[0].offsetWidth;
+
+      el.setAttribute('data-width', textWidth + 41);
+
+      el.addEventListener('mouseover', function() {
+        this.style.width = this.getAttribute('data-width') + 'px';
+      }, false);
+      el.addEventListener('focus', function() {
+        this.style.width = this.getAttribute('data-width') + 'px';
+      }, false);
+      el.addEventListener('mouseout', function() {
+        this.style.width = '';
+      }, false);
+      el.addEventListener('focusout', function() {
+        this.style.width = '';
+      }, false);
+    });
+
+    // Form
+
+    input.addEventListener('keyup', function() {
+      this.setAttribute('data-val', this.value);
+    }, false);
+
+    submit.addEventListener('click', function() {
+      var invalid = document.querySelectorAll('input[type="email"]:invalid')[0];
+      if(invalid) {
+        invalid.style.animationName = 'shake';
+        invalid.style.animationDuration = '.3s';
+      } else {
+        var logo = document.querySelectorAll('.logo + svg')[0];
+        logo.style.animationName = 'logo';
+        logo.style.animationDuration = '1.5s';
+        input.style.pointerEvents = 'none';
+        input.tabIndex = -1;
+        submit.style.opacity = 0.5;
+        submit.style.pointerEvents = 'none';
+        submit.style.backgroundColor = '#6441a5';
+        submit.tabIndex = -1;
+        submit.value = 'Sending request...';
+        submit.blur();
+        setTimeout(function() {
+          submit.value = 'Thanks for subscribing';
+          submit.style.opacity = 1;
+          form.style.cursor = 'not-allowed';
+        }, 3000);
+        setTimeout(function() {
+          form.style.opacity = 0.5;
+        }, 5000);
+      }
+    }, false);
+
+    submit.addEventListener('mousedown', function() {
+      if (form.classList) {
+        form.classList.add('active');
+      } else {
+        form.className += ' ' + 'active';
+      }
+    }, false);
+
+    submit.addEventListener('mouseup', function() {
+      if (form.classList) {
+        form.classList.remove('active');
+      } else {
+        form.className = form.className.replace(new RegExp('(^|\\b)' + 'active'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+      }
+    }, false);
+
+    input.addEventListener('animationend', function() {
+      this.style.animationName = '';
+      this.style.animationDuration = '';
+    }, false);
+
+    input.addEventListener('webkitAnimationEnd', function() {
+      this.style.animationName = '';
+      this.style.animationDuration = '';
+    }, false);
+
+  });
+
+}());
+
+/* jQuery version (2115 bytes minified + ~100KB library ;))
+
+(function($, window, document) {
+
+  'use strict';
+
+  // Page loader
+
+  $(window).load(function() {
+
+    var $body = $('body'),
+      $loader = $body.find('.loader-wrapper');
+
+    setTimeout(function() {
+      $loader.css({
+        opacity: 0,
+        visibility: 'hidden'
+      });
+      $body.css('overflow-y', 'auto');
+    }, 500);
+
+  });
+
+  // On DOM ready
+
+  $(function() {
+
+    // Variables
+
+    var $body = $('body'),
+      $sidebar = $body.find('.sidebar'),
+      $sidebarToggle = $body.find('.sidebar-toggle'),
+      $menu = $body.find('.sidebar ul'),
+      $menuElements = $menu.find('li'),
+      $menuElementLine = $body.find('.sidebar-link-line'),
+      $menuActiveElement = $menu.find('.active'),
+      $socialButtons = $body.find('.social > a'),
+      $form = $body.find('form'),
+      $input = $form.find('input[type="email"]'),
+      $submit = $form.find('input[type="submit"]');
+
+
+    // Close sidebar on resize (if browser width > 992px)
+
+    $(window).resize(function() {
+
+      if ($(window).width() > 992) {
+        $body.removeClass('sidebar-opened');
+      }
+
+    });
+
+    // Mobile menu toggle
+
+    $sidebarToggle.click(function() {
+
+      $body.toggleClass('sidebar-opened');
+      $sidebar.addClass('active');
+
+    });
+
+    $sidebar.on('webkitTransitionEnd transitionend', function() {
+      $(this).removeClass('active');
+    });
+
+    // Close mobile menu on clicking/tapping outside and on ESC
+
+    $(document).on('mouseup touchend keyup', function(event) {
+
+      if (event.keyCode === 27) {
+        $sidebar.addClass('active');
+        $body.removeClass('sidebar-opened');
+      }
+
+      if($('.sidebar-overlay').is(event.target)) {
+        $sidebar.addClass('active');
+        $body.removeClass('sidebar-opened');
+      }
+
+    });
+
+    // Menu link hover/focus effect
+
+    $menuElements.on('mouseenter focusin', function() {
+      $menuElementLine.css('top', $(this).position().top);
+    });
+    $menuElements.on('mouseleave focusout', function() {
+      $menuElementLine.css('top', $menuActiveElement.position().top);
+    });
+
+    // Social button hover/focus effect
+
+    $socialButtons.each(function() {
+      var $textWidth = $(this).find('.text').outerWidth();
+      $(this).attr('data-width', $textWidth + 41);
+    });
+
+    $socialButtons.on('mouseover focusin', function() {
+      $(this).css('width', $(this).attr('data-width'));
+    });
+
+    $socialButtons.on('mouseleave focusout', function() {
+      $(this).css('width', '');
+    });
+
+    // Form
+
+    $input.keyup(function() {
+      $(this).attr('data-val', $(this).val());
+    });
+
+    $submit.click(function() {
+
+      if($input.is(':invalid')) {
+        $input.css({
+          animationName: 'shake',
+          animationDuration: '.3s'
+        });
+      } else {
+        var $logo = $('.logo + svg');
+        $logo.css({
+          animationName: 'logo',
+          animationDuration: '1.5s'
+        });
+        $input.css('pointer-events', 'none')
+          .attr('tabindex', -1);
+        $submit.css({
+          opacity: 0.5,
+          pointerEvents: 'none',
+          backgroundColor: '#6441a5'
+        }).attr('tabindex', -1)
+          .val('Sending request...')
+          .blur();
+        setTimeout(function() {
+          $submit.css('opacity', 1)
+            .attr('tabindex', -1)
+            .val('Thanks for subscribing');
+          $form.css('cursor', 'not-allowed');
+        }, 3000);
+        setTimeout(function() {
+          $form.css('opacity', '.5');
+        }, 5000);
+      }
+
+    });
+
+    $submit.mousedown(function() {
+      $form.addClass('active');
+    });
+
+    $submit.mouseup(function() {
+      $form.removeClass('active');
+    });
+
+    $input.on('webkitAnimationEnd animationend', function() {
+      $(this).css({
+        animationName: '',
+        animationDuration: ''
+      });
+    });
+
+  });
+
+}(window.jQuery, window, document));
+
+*/
